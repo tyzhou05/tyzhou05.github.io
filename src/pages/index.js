1b193512a16e48cd41cc1435aa2c3ce5cb601ed5
Tony Zhou <tyzhou05@uw.edu>
2024-07-02 22:51:17 -0700

    update github

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,7 +134,7 @@ const IndexPage = () => (
           LinkedIn: <a href = "https://www.linkedin.com/in/tyzhou">tyzhou</a> 
         </li>
         <li>
-          GitHub: <a href = "mailto:tyzhou05@uw.edu">tyzhou05@uw.edu</a> 
+          GitHub: <a href = "https://www.github.com/tyzhou05">tyzhou05</a> 
         </li>
         <li>
           Publications: <a href = "https://scholar.google.com/citations?user=S4zK_nsAAAAJ&hl=en">Google Scholar</a>
